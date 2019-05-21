16ecdda0efbcbfefc45bf15b201b975b963eac95
Ivan Bedecarats <igbedecarats@gmail.com>
2019-05-21 18:32:15 -0300

    Added product-storage to docker-compose.yaml

--- product-storage/src/main/java/com/gm/storage/StorageApplication.java
+++ product-storage/src/main/java/com/gm/storage/StorageApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductStorageApplication {
+public class StorageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductStorageApplication.class, args);
+		SpringApplication.run(StorageApplication.class, args);
 	}
 
 }
